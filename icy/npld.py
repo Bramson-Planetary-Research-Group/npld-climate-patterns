1f742911f8f729cc2bc5aab2b5cde0f775169ae8
Tom McClintock <tmcclintock89@gmail.com>
2022-11-13 17:02:57 -0800

    fix: placeholder function

--- icy/npld.py
+++ icy/npld.py
@@ -1,1 +1,6 @@
 """Placeholder file until real code gets written."""
+
+
+def placeholder_function():
+    """Placeholder function that prints."""
+    print("I am a placeholder. Can't want to go to Mars!")
--- tests/test_icy.py
+++ tests/test_icy.py
@@ -1,4 +1,12 @@
+from icy.npld import placeholder_function
+
+
 def test_smoke():
     import icy
 
     assert icy is not None
+
+
+def test_placeholder():
+    """Delete me asap."""
+    placeholder_function()
